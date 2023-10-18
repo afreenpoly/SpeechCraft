45732718a27aa15af0daba787481c0fc9c25845e
Aswin K <aswinkarat@gmail.com>
2023-10-18 15:47:23 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/homepage/Home";
+import User from "./pages/user/User";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route index element={<Home />}></Route>
+        <Route path="/user" element={<User />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
