2ef81ace8b4b5af088392fb4aa084195de6b9f8c
Aswin K <aswinkarat@gmail.com>
2023-10-22 16:10:27 +0530

    Updated UI/UX

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { UserOutlined } from "@ant-design/icons";
+import { Popover } from "antd";
 
 const Navbar = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -7,6 +9,12 @@ const Navbar = () => {
   const toggleProfileDropdown = () => {
     setIsProfileOpen(!isProfileOpen);
   };
+  const content = (
+    <div className="flex flex-col w-20 text-center space-y-3">
+      <Link to="/user/profile">Profile</Link>
+      <Link to="/">Logout</Link>
+    </div>
+  );
 
   return (
     <nav className="bg-blue-500 p-4">
@@ -16,44 +24,16 @@ const Navbar = () => {
         </Link>
 
         <div className="relative">
-          <button
-            onClick={toggleProfileDropdown}
-            className="flex items-center text-white focus:outline-none"
-          >
-            <div className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-8 h-8"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 15v5a7 7 0 0 0 14 0v-5"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 14c-2.761 0-5-2.239-5-5s2.239-5 5-5 5 2.239 5 5-2.239 5-5 5z"
-                />
-              </svg>
-            </div>
-          </button>
-          {isProfileOpen && (
-            <div className="absolute flex flex-col right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg">
-              <Link className="px-4 py-2 hover:bg-blue-100" to="/user/profile">
-                Profile
-              </Link>
-
-              <Link className="px-4 py-2 hover:bg-blue-100" to="/">
-                Logout
-              </Link>
-            </div>
-          )}
+          <Popover content={content}>
+            <button
+              onClick={toggleProfileDropdown}
+              className="flex items-center text-white focus:outline-none"
+            >
+              <div className="w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center">
+                <UserOutlined />
+              </div>
+            </button>
+          </Popover>
         </div>
       </div>
     </nav>
--- client/src/pages/user/dashboard/Dashboard.jsx
+++ client/src/pages/user/dashboard/Dashboard.jsx
@@ -1,33 +1,83 @@
 import React from "react";
-import Card from "../../../components/Card";
+import { Link } from "react-router-dom";
+import { Card, Calendar, Divider } from "antd";
 
 function Dashboard() {
   return (
     <div className="pb-4">
       {/* First Section */}
       <h2 className="text-2xl font-semibold text-center mb-4">Learning</h2>
-      <div className="flex space-x-36 justify-center">
-        <Card name="Alphabets" color="red" link="/user/learning/alphabets" />
-        <Card name="Words" color="blue" link="/user/learning/words" />
-        <Card name="Sentences" color="green" link="/user/learning/sentences" />
+      <div className="grid grid-cols-3 justify-center justify-items-center">
+        <Link to={"/user/learning/alphabets"}>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={<img alt="alphabets" src="./assets/alphabets.jpg" />}
+          >
+            <div className="text-2xl text-center font-semibold">Alphabets</div>
+          </Card>
+        </Link>
+        <Link to={"/user/learning/words"}>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={<img alt="words" src="./assets/words.jpg" />}
+          >
+            <div className="text-2xl text-center font-semibold">Words</div>
+          </Card>
+        </Link>
+        <Link to={"/user/learning/words"}>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={<img alt="words" src="./assets/words.jpg" />}
+          >
+            <div className="text-2xl text-center font-semibold">Sentences</div>
+          </Card>
+        </Link>
       </div>
 
       {/* Divider Line */}
-      <div className="border-b border-black border-4 my-6" />
+      <Divider className="border-2" />
 
       {/* Second Section */}
       <h2 className="text-2xl font-semibold text-center my-6"> Practice</h2>
-      <div className="flex space-x-36 justify-center">
-        <Card
-          name="Flash Card"
-          color="green"
-          link="/user/challenges/flashCard"
-        />
-        <Card
-          name="Multiplayer Mode"
-          color="red"
-          link="/user/challenges/multiplayer"
-        />
+      <div className="grid grid-cols-3 justify-center justify-items-center">
+        <Link to={"/user/challenges/flashcard"}>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={<img alt="flashcard" src="./assets/flashcard.jpg" />}
+          >
+            <div className="text-2xl text-center font-semibold">Flash Card</div>
+          </Card>
+        </Link>
+
+        <Link to={"/user/challenges/multiplayer"}>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            cover={<img alt="multiplayer" src="./assets/multiplayer.jpg" />}
+          >
+            <div className="text-2xl text-center font-semibold">
+              Multiplayer
+            </div>
+          </Card>
+        </Link>
+
+        <div className={`w-80 border-1 border-solid `}>
+          <Calendar fullscreen={false} />
+        </div>
       </div>
     </div>
   );
