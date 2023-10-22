96cb1436354b589c8089434cd8dfbc45776875ef
Aswin K <aswinkarat@gmail.com>
2023-10-22 21:01:19 +0530

    Updated flash card

--- client/src/components/SpinningCard.jsx
+++ client/src/components/SpinningCard.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
-const SpinningCard = ({ Content }) => {
+const SpinningCard = ({ frontContent, backContent, spin }) => {
   return (
-    <div
-      className={`w-40 h-60 relative transform transition-transform duration-500`}
-    >
-      <div className="w-full h-full absolute flex justify-center items-center bg-red-500 text-white text-2xl font-semibold">
-        {Content}
+    <div className="w-60 h-60 relative transform transition-transform duration-500">
+      <div></div>
+      <div className="group h-full w-full [perspective:1000px]">
+        <div
+          className={`relative h-full w-full border border-6 rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] ${
+            spin ? "[transform:rotateY(180deg)]" : ""
+          }`}
+        >
+          <div className="absolute inset-0 text-center bg-lime-400 rounded-xl">
+            <div className="min-h-full flex justify-center text-center items-center">
+              <h1 className="text-3xl font-bold">{frontContent}</h1>
+            </div>
+          </div>
+          {spin && (
+            <div className="absolute inset-0  h-full w-full rounded-xl bg-cyan-500 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+              <div className=" min-h-full flex justify-center text-center items-center ">
+                <h1 className="text-3xl font-bold">{backContent}</h1>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
